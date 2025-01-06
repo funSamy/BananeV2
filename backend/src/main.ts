8c5c5190f7ee121360e597f535446285bdb06fac
Joy-sameza <101399643+Joy-sameza@users.noreply.github.com>
2025-01-06 08:19:56 +0100

    - Added authentication module with controller, service, and DTOs for user management. - Integrated Prisma for database management with SQLite, including schema and initial migration setup. - Updated app module to include configuration and Prisma modules. - Removed unused app controller and service files to streamline the codebase.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable versioning with /api/v1 prefix
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
